7a57c214b3ed93073338f963d811e09b6deb637b
Priyanshu Rai <idealpriyanshu@gmail.com>
2022-04-04 21:11:31 +0530

    major

---
+++ bfs_sum.c
@@ -0,0 +1,28 @@
+// Print the queue
+void printQueue(struct queue* q) {
+  int i = q->front;
+
+  if (isEmpty(q)) {
+    printf("Queue is empty");
+  } else {
+    printf("\nQueue contains \n");
+    for (i = q->front; i < q->rear + 1; i++) {
+      printf("%d ", q->items[i]);
+    }
+  }
+}
+
+int main() {
+  struct Graph* graph = createGraph(6);
+  addEdge(graph, 0, 1);
+  addEdge(graph, 0, 2);
+  addEdge(graph, 1, 2);
+  addEdge(graph, 1, 4);
+  addEdge(graph, 1, 3);
+  addEdge(graph, 2, 4);
+  addEdge(graph, 3, 4);
+
+  bfs(graph, 0);
+
+  return 0;
+}
